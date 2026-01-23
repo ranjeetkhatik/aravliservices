d16c2523265d34c0c43e4454a7251b28b1a58c0a
Ranjeet <ranjeetkhatik90@gmail.com>
2026-01-23 17:05:08 +0530

    making first component  1

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Homepage from './components/Homepage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Homepage />
     </>
   )
 }
---
+++ src/components/Homepage.jsx
@@ -0,0 +1,12 @@
+function Homepage() {
+  return (
+    <div className="homepage-container">
+      {/* This is where our sections will go */}
+      <header>
+        <h1>Welcome to My Website</h1>
+      </header>
+    </div>
+  );
+}
+
+export default Homepage;
