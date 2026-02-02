9e80be4c036aaaff6341f68ac59e697f3c6f8d06
ranjeetkhatik <ranjeetkhatik90@gmail.com>
2026-02-02 21:41:29 +0530

    1

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -10,12 +10,14 @@ function ContactForm() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("Form Submitted:", formData);   
-  };
+    console.log("Form Submitted:", formData);
+  alert("संदेश सफलतापूर्वक भेज दिया गया! ✅");
+  }
 
   return (
    
    <div className="min-h-screen bg-gray-100 flex items-center justify-center p-6">
+
       <div className="w-full max-w-4xl bg-white rounded-xl shadow-lg overflow-hidden flex flex-col md:flex-row">
         
         {/* बायां हिस्सा: संपर्क जानकारी (Blue Section) */}
