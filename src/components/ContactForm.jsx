275c873e19994c531253c0d8ad2a88663906aad6
ranjeetkhatik <ranjeetkhatik90@gmail.com>
2026-02-02 20:22:03 +0530

    heleo

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -35,7 +35,9 @@ function ContactForm() {
   };
 
   return (
+   
    <div className="min-h-screen bg-gray-100 flex items-center justify-center p-6">
+     <h1>Hello ranjeet</h1>
       <div className="w-full max-w-4xl bg-white rounded-xl shadow-lg overflow-hidden flex flex-col md:flex-row">
         
         {/* बायां हिस्सा: संपर्क जानकारी (Blue Section) */}
